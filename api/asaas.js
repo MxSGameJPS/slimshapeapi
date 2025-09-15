2a10897c569535b4e384f2e056eeb3bd7d5e979b
MxSGameJPS <mxsgamejps@gmail.com>
2025-09-15 13:27:25 -0300

    refactor: ajustar middleware CORS para definir origem padrão

--- api/asaas.js
+++ api/asaas.js
@@ -15,8 +15,6 @@ function corsHandler(req, res) {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-  } else {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigins[0]);
   }
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
